14e7975a147429962a76f8233db0a87091e19968
Ira Rainey <irarainey@microsoft.com>
2022-10-06 12:18:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization(o =>
+{
+    // Create and authorisation policy based upon a specific scope to say hello
+    o.AddPolicy("HelloCaller", new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .RequireClaim("http://schemas.microsoft.com/identity/claims/scope", builder.Configuration["AzureAd:HelloScope"])
+        .Build());
+});
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Hello endpoint which implements the HelloCaller authorisation policy
+app.MapGet("/hello", (HttpContext httpContext) =>
+{
+    var name = httpContext.User.Claims.First(c => c.Type == "name").Value;
+
+    return $"Hello {name}";
+})
+.RequireAuthorization("HelloCaller");
+
+// Goodbye endpoint which uses an alternative mechanism (VerifyUserHasAnyAcceptedScope) to check for the required scope
+app.MapGet("/goodbye", (HttpContext httpContext) =>
+{
+    httpContext.VerifyUserHasAnyAcceptedScope(new string[] { builder.Configuration["AzureAd:GoodbyeScope"] });
+
+    var name = httpContext.User.Claims.First(c => c.Type == "name").Value;
+
+    return "Goodbye {name}";
+})
+.RequireAuthorization();
+
+app.Run();
